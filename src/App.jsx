2fb1a61d282501948fc7971d22cc8b100a497add
vishalparashar0119 <vishalparashar0119@users.noreply.github.com>
2025-11-25 11:09:48 +0530

    setuping some basic pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className="absolute top-5 left-1/2 -translate-x-1/2 -translate-y-1/2 p-3 rounded-md bg-red-500">
+            <span className="inline-block mt-1 mb-1 text-white">
+                somthing went wrong , please try again
+            </span>
+        </div>
+        
+      <div className="w-full h-screen flex px-20">
+        <div className="w-1/2 flex items-center justify-center h-screen">
+          <div className="w-full px-32">
+            <h3 className="text-4xl">welcome to <span className="text-blue-400 font-semibold">Scatch</span></h3>
+            <h4 className="text-2xl mb-5">create your account</h4>
+            <form autocomplete="off" action="/users/register" method="post">
+              <input
+                className="bg-zinc-100 block w-full px-3 py-2 border rounded-md mb-3 border-zinc-200"
+                type="text" placeholder="Full Name" name="fullname" />
+              <input
+                className="bg-zinc-100 block w-full px-3 py-2 border rounded-md mb-3 border-zinc-200"
+                type="email" placeholder="Email" name="email" />
+              <input
+                className="bg-zinc-100 block w-full px-3 py-2 border rounded-md mb-3 border-zinc-200"
+                type="password" placeholder="Password" name="password" />
+              <input className="px-5 rounded-full py-3 mt-2 bg-blue-500 text-white" type="submit"
+                value="Create My Account" />
+            </form>
+          </div>
+        </div>
+        <div className="w-1/2 flex items-center justify-center h-screen">
+          <div className="w-full px-32">
+            <h4 className="text-2xl capitalize mb-5">login your account</h4>
+            <form autocomplete="off" action="/users/login" method="post">
+              <input
+                className="block bg-zinc-100 w-full px-3 py-2 border rounded-md mb-3 border-zinc-200"
+                type="email" placeholder="Email" name="email" />
+              <input
+                className="block bg-zinc-100 w-full px-3 py-2 border rounded-md mb-3 border-zinc-200"
+                type="password" placeholder="Password" name="password" />
+              <input className="px-5 block rounded-full py-3 mt-2 bg-blue-500 text-white" type="submit"
+                value="Login" />
+            </form>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Shop from './pages/shop.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Shop/>
   </StrictMode>,
 )

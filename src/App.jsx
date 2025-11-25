4d967d7f16b088ec94fb7704f758d0d7f52d6079
vishalparashar0119 <vishalparashar0119@users.noreply.github.com>
2025-11-25 18:20:01 +0530

    intrigate react hook form and zod for  form validtion

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { useForm } from 'react-hook-form'
 import './App.css'
+import {z} from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 function App() {
 
+  const registerSchema = z.object({
+    fullName : z.string().min(1 , 'Name must be at least 1 character long'),
+    email : z.email('Invali email formate'),
+    password : z.string().min(6 , 'Passowrd must me 6 digit long'),
+  })
+  
+  const loginSchema = z.object({
+    email : z.email('Invali email formate'),
+    password : z.string().min(6 , 'Passowrd must me 6 digit long'),
+  })
+
   const { register, formState: { errors, isSubmitting }, handleSubmit } = useForm({
-    defaultValues: {
-      fullName: '',
-      email: '',
-      password: ''
-    }
+     resolver : zodResolver(registerSchema)
   });
 
   const onSubmit = async (data) => {
@@ -20,13 +29,32 @@ function App() {
 
   }
 
+  const { register: loginRegister, formState: {
+    isSubmitting: loginIsSubmitting, errors: loginErrors
+  }, handleSubmit: handleLoginSubmit } = useForm({
+    resolver : zodResolver(loginSchema)
+  })
+
+  const onLoginSubmit = async (data) => {
+    console.log("Form data:", data);
+
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    console.log("Done!");
+
+  }
+
   return (
     <>
-      <div className="absolute top-5 left-1/2 -translate-x-1/2 -translate-y-1/2 p-3 rounded-md bg-red-500">
-        <span className="inline-block mt-1 mb-1 text-white">
-          somthing went wrong , please try again
-        </span>
-      </div>
+      {
+        errors.root && (
+          <div className="absolute top-5 left-1/2 -translate-x-1/2 -translate-y-1/2 p-3 rounded-md bg-red-500">
+            <span className="inline-block mt-1 mb-1 text-white">
+              somthing went wrong , please try again
+            </span>
+          </div>
+        )
+      }
 
       <div className="w-full h-screen flex px-20">
         <div className="w-1/2 flex items-center justify-center h-screen">
@@ -38,7 +66,7 @@ function App() {
               {/* full name  */}
               <input
                 className="bg-zinc-100 block w-full px-3 py-2 border rounded-md mb-3 border-zinc-200" type='text'
-                placeholder="Full Name"  {...register('fullName', { required: 'full name is require' })} />
+                placeholder="Full Name"  {...register('fullName')} />
               {
                 errors.fullName && (
                   <p className='text-orange-500'>{errors.fullName.message}</p>
@@ -48,12 +76,7 @@ function App() {
               {/* email  */}
               <input
                 className="bg-zinc-100 block w-full px-3 py-2 border rounded-md mb-3 border-zinc-200"
-                placeholder="Email" type='text' {...register('email', {
-                  required: 'email is require', pattern: {
-                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-                    , message: 'invalid email address'
-                  }
-                })} />
+                placeholder="Email" type='text' {...register('email')} />
               {
                 errors.email && (
                   <p className='text-orange-500'>{errors.email.message}</p>
@@ -63,7 +86,7 @@ function App() {
               {/* password */}
               <input
                 className="bg-zinc-100 block w-full px-3 py-2 border rounded-md mb-3 border-zinc-200"
-                type="password" placeholder="Password" {...register('password', { required: 'password is require' })} />
+                type="password" placeholder="Password" {...register('password')} />
               {
                 errors.password && (
                   <p className='text-orange-500'>{errors.password.message}</p>
@@ -83,15 +106,33 @@ function App() {
         <div className="w-1/2 flex items-center justify-center h-screen">
           <div className="w-full px-32">
             <h4 className="text-2xl capitalize mb-5">login your account</h4>
-            <form autocomplete="off" action="/users/login" method="post">
+            <form autoComplete="off" onSubmit={handleLoginSubmit(onLoginSubmit)}>
+              {/* email */}
               <input
                 className="block bg-zinc-100 w-full px-3 py-2 border rounded-md mb-3 border-zinc-200"
-                type="email" placeholder="Email" name="email" />
+                type="email" placeholder="Email" {...loginRegister('email', {
+                  required: ' email is require', pattern: {
+                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+                    , message: 'invalid email address'
+                  }
+                })} />
+              {
+                loginErrors.password && (
+                  <p className='text-orange-500'>{loginErrors.email.message}</p>
+                )
+              }
+              {/* password */}
               <input
                 className="block bg-zinc-100 w-full px-3 py-2 border rounded-md mb-3 border-zinc-200"
-                type="password" placeholder="Password" name="password" />
-              <input className="px-5 block rounded-full py-3 mt-2 bg-blue-500 text-white" type="submit"
-                value="Login" />
+                type="password" placeholder="Password"  {...loginRegister('password', { required: 'password is require' })} />
+              {
+                loginErrors.password && (
+                  <p className='text-orange-500'>{loginErrors.password.message}</p>
+                )
+              }
+              <button className="px-5 block rounded-full py-3 mt-2 bg-blue-500 text-white" type="submit"
+                disabled={loginIsSubmitting}>
+                {loginIsSubmitting ? 'loging...' : 'Login'} </button>
             </form>
           </div>
         </div>

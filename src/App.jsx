77ea53a7187f0c6cc9ddcd0a30994d60665eac68
vishalparashar0119 <itsvishalparashar0119@gmail.com>
2025-11-28 19:33:24 +0530

    created admin page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { Routes , Route } from 'react-router-dom';
 import Login from './pages/login.jsx';
 import Shop from './pages/shop.jsx'
+import Admin from './pages/admin.jsx';
+
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Login/>}/>
       <Route path='/shop' element={<Shop/>}/>
+      <Route path='/admin' element={<Admin/>}/>
+      
     </Routes>
   )
 }

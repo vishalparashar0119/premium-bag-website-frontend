34d89dbc254b040dd4ee446b8d3e7cc1a2723698
vishalparashar0119 <itsvishalparashar0119@gmail.com>
2025-12-01 23:02:10 +0530

    added logout functionality

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,22 @@
+import axios from 'axios'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { Bounce, toast , ToastContainer } from 'react-toastify'
 
 const Navbar = () => {
+
+      const navigate = useNavigate();
+      const handleLogout = async () => {
+
+            try {
+                  const  confirmLogout = await axios.post('http://localhost:3000/users/logout' , {} , {withCredentials:true});
+                  if(confirmLogout.data.success) navigate('/');
+
+            } catch ( error) {
+                    toast.error(error.confirmLogout.data.message)
+            }
+      }
       return (
             <div className="">
                   <nav className="w-full fixed top-0 left-0 px-5 py-3 flex justify-between items-center ">
@@ -11,9 +26,10 @@ const Navbar = () => {
                               <Link to='/shop'>Shop</Link>
                               <Link to='/cart'>Cart</Link>
                               <Link to='/myAccount'>My Account</Link>
-                              <button className='bg-red-600 text-white cursor-pointer py-1 px-3 rounded-sm'>Logout</button>
+                              <button className='bg-red-600 text-white cursor-pointer py-1 px-3 rounded-sm' onClick={handleLogout}>Logout</button>
                         </div>
                   </nav>
+                  <ToastContainer position='top-center' transition={Bounce}/>
             </div>
       )
 }

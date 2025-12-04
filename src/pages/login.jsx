8ee9a9ecca1829bbe8d3dc7452481fdfeae6da8d
vishalparashar0119 <itsvishalparashar0119@gmail.com>
2025-12-04 19:55:51 +0530

    fixed login page with different component

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -4,27 +4,46 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { Bounce, ToastContainer, toast } from 'react-toastify'
+import { useState } from 'react';
+import { MdToll } from 'react-icons/md';
 
 function Login() {
+  const [toggle, setToggle] = useState(true)
 
-  const navigate = useNavigate();
+
+  return (
+    <>
+      <div className="w-full h-screen flex px-20  flex-col justify-center  items-center">
+
+
+        {toggle ? <CreateComponent /> : <LoginComponent />}
+
+        {/*  */}
+        <div className='text-start  mt-3 w-1/2 px-33'>
+        
+          <button onClick={() => setToggle(!toggle)} className='text-blue-400 cursor-pointer'>{toggle ? "have an accout ?" : "dont have account ?"}</button>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+const LoginComponent = () => {
+
 
   const notify = (message) => toast.error(message)
+  const navigate = useNavigate();
 
-  const registerSchema = z.object({
-    fullName: z.string().min(1, 'Name must be at least 1 character long'),
-    email: z.email('Invali email formate'),
-    password: z.string().min(6, 'Passowrd must me 6 digit long'),
-  })
+
+
+
 
   const loginSchema = z.object({
     email: z.email('Invali email formate'),
     password: z.string().min(6, 'Passowrd must me 6 digit long'),
   })
 
-  const { register, formState: { errors, isSubmitting }, handleSubmit } = useForm({
-    resolver: zodResolver(registerSchema)
-  });
 
   const { register: loginRegister, formState: {
     isSubmitting: loginIsSubmitting, errors: loginErrors
@@ -32,23 +51,6 @@ function Login() {
     resolver: zodResolver(loginSchema)
   })
 
-  const onSubmit = async (data) => {
-
-    try {
-      const res = await axios.post('http://localhost:3000/users/register', { fullName: data.fullName, email: data.email, password: data.password }, {
-        withCredentials: true
-      })
-
-      if (res.data.success) navigate('/shop');
-
-      notify(res.data.message)
-      console.log(res)
-    } catch (error) {
-      notify(error.res.data.message)
-    }
-
-    await new Promise(resolve => setTimeout(resolve, 2000));
-  }
 
 
   const onLoginSubmit = async (data) => {
@@ -69,84 +71,131 @@ function Login() {
     console.log("Done!");
 
   }
+  return (
+
+    <div className="w-1/2 flex items-center justify-center h-fit">
+      <div className="w-full px-32">
+        <h3 className="text-4xl">welcome to <span className="text-blue-400 font-semibold">Scatch</span></h3>
+        <h4 className="text-2xl capitalize mb-5">login your account</h4>
+        <form autoComplete="off" onSubmit={handleLoginSubmit(onLoginSubmit)}>
+          {/* email */}
+          <input
+            className="block bg-zinc-100 w-full px-3 py-2 border rounded-md mb-3 border-zinc-200"
+            type="email" placeholder="Email" {...loginRegister('email')} />
+          {
+            loginErrors.password && (
+              <p className='text-orange-500'>{loginErrors.email.message}</p>
+            )
+          }
+          {/* password */}
+          <input
+            className="block bg-zinc-100 w-full px-3 py-2 border rounded-md mb-3 border-zinc-200"
+            type="password" placeholder="Password"  {...loginRegister('password')} />
+          {
+            loginErrors.password && (
+              <p className='text-orange-500'>{loginErrors.password.message}</p>
+            )
+          }
+          <button className="px-5 block rounded-full py-3 mt-2 bg-blue-500 text-white" type="submit"
+            disabled={loginIsSubmitting}>
+            {loginIsSubmitting ? 'loging...' : 'Login'} </button>
+        </form>
+      </div>
+
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={true}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce} />
+    </div>
+  )
+}
+
+const CreateComponent = () => {
+
+
+  const navigate = useNavigate();
+
+  const notify = (message) => toast.error(message)
+
+  const registerSchema = z.object({
+    fullName: z.string().min(1, 'Name must be at least 1 character long'),
+    email: z.email('Invali email formate'),
+    password: z.string().min(6, 'Passowrd must me 6 digit long'),
+  })
+
+  const { register, formState: { errors, isSubmitting }, handleSubmit } = useForm({
+    resolver: zodResolver(registerSchema)
+  });
+
+  const onSubmit = async (data) => {
+
+    try {
+      const res = await axios.post('http://localhost:3000/users/register', { fullName: data.fullName, email: data.email, password: data.password }, {
+        withCredentials: true
+      })
+
+      if (res.data.success) navigate('/shop');
+
+      notify(res.data.message)
+      console.log(res)
+    } catch (error) {
+      notify(error.res.data.message)
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 2000));
+  }
 
   return (
-    <>
-      <div className="w-full h-screen flex px-20">
-        <div className="w-1/2 flex items-center justify-center h-screen">
-          <div className="w-full px-32">
-            <h3 className="text-4xl">welcome to <span className="text-blue-400 font-semibold">Scatch</span></h3>
-            <h4 className="text-2xl mb-5">create your account</h4>
-            <form autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+    <div className="w-1/2 flex items-center justify-center h-fit">
+      <div className="w-full px-32">
+        <h3 className="text-4xl">welcome to <span className="text-blue-400 font-semibold">Scatch</span></h3>
+        <h4 className="text-2xl mb-5">create your account</h4>
+        <form autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
 
-              {/* full name  */}
-              <input
-                className="bg-zinc-100 block w-full px-3 py-2 border rounded-md mb-3 border-zinc-200" type='text'
-                placeholder="Full Name"  {...register('fullName')} />
-              {
-                errors.fullName && (
-                  <p className='text-orange-500'>{errors.fullName.message}</p>
-                )
-              }
+          {/* full name  */}
+          <input
+            className="bg-zinc-100 block w-full px-3 py-2 border rounded-md mb-3 border-zinc-200" type='text'
+            placeholder="Full Name"  {...register('fullName')} />
+          {
+            errors.fullName && (
+              <p className='text-orange-500'>{errors.fullName.message}</p>
+            )
+          }
 
-              {/* email  */}
-              <input
-                className="bg-zinc-100 block w-full px-3 py-2 border rounded-md mb-3 border-zinc-200"
-                placeholder="Email" type='text' {...register('email')} />
-              {
-                errors.email && (
-                  <p className='text-orange-500'>{errors.email.message}</p>
-                )
-              }
+          {/* email  */}
+          <input
+            className="bg-zinc-100 block w-full px-3 py-2 border rounded-md mb-3 border-zinc-200"
+            placeholder="Email" type='text' {...register('email')} />
+          {
+            errors.email && (
+              <p className='text-orange-500'>{errors.email.message}</p>
+            )
+          }
 
-              {/* password */}
-              <input
-                className="bg-zinc-100 block w-full px-3 py-2 border rounded-md mb-3 border-zinc-200"
-                type="password" placeholder="Password" {...register('password')} />
-              {
-                errors.password && (
-                  <p className='text-orange-500'>{errors.password.message}</p>
-                )
-              }
+          {/* password */}
+          <input
+            className="bg-zinc-100 block w-full px-3 py-2 border rounded-md mb-3 border-zinc-200"
+            type="password" placeholder="Password" {...register('password')} />
+          {
+            errors.password && (
+              <p className='text-orange-500'>{errors.password.message}</p>
+            )
+          }
 
-              {/* submit */}
-              <button className="px-5 rounded-full py-3 mt-2 bg-blue-500 text-white" type="submit"
-                disabled={isSubmitting}>
-                {isSubmitting ? 'Creating...' : 'Create My Account'}
-              </button>
-            </form>
-          </div>
-        </div>
-
-
-        <div className="w-1/2 flex items-center justify-center h-screen">
-          <div className="w-full px-32">
-            <h4 className="text-2xl capitalize mb-5">login your account</h4>
-            <form autoComplete="off" onSubmit={handleLoginSubmit(onLoginSubmit)}>
-              {/* email */}
-              <input
-                className="block bg-zinc-100 w-full px-3 py-2 border rounded-md mb-3 border-zinc-200"
-                type="email" placeholder="Email" {...loginRegister('email')} />
-              {
-                loginErrors.password && (
-                  <p className='text-orange-500'>{loginErrors.email.message}</p>
-                )
-              }
-              {/* password */}
-              <input
-                className="block bg-zinc-100 w-full px-3 py-2 border rounded-md mb-3 border-zinc-200"
-                type="password" placeholder="Password"  {...loginRegister('password')} />
-              {
-                loginErrors.password && (
-                  <p className='text-orange-500'>{loginErrors.password.message}</p>
-                )
-              }
-              <button className="px-5 block rounded-full py-3 mt-2 bg-blue-500 text-white" type="submit"
-                disabled={loginIsSubmitting}>
-                {loginIsSubmitting ? 'loging...' : 'Login'} </button>
-            </form>
-          </div>
-        </div>
+          {/* submit */}
+          <button className="px-5 rounded-full py-3 mt-2 bg-blue-500 text-white" type="submit"
+            disabled={isSubmitting}>
+            {isSubmitting ? 'Creating...' : 'Create My Account'}
+          </button>
+        </form>
       </div>
       <ToastContainer
         position="top-center"
@@ -160,7 +209,7 @@ function Login() {
         pauseOnHover
         theme="light"
         transition={Bounce} />
-    </>
+    </div>
   )
 }
 

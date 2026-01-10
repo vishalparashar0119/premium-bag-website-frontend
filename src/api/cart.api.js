aacb7d84db7d78e166b6c980a85f1b91b4706cba
vishalparashar0119 <itsvishalparashar0119@gmail.com>
2026-01-10 10:19:07 +0530

    crete a sepreat api for add to cart api

--- src/api/cart.api.js
+++ src/api/cart.api.js
@@ -1,3 +1,18 @@
 // add to cart 
 // remove from cart
 // fetch cart items
+
+import axios from "axios";
+import { toast } from "react-toastify";
+import { BACKEND_URL } from "../config/env";
+
+export const addToCart = async (id ) =>{
+      try {
+            const response = await axios.post(`${BACKEND_URL}/users/addToCart/${id}`, {}, {
+                  withCredentials: true
+            });
+            toast.success(response.data.message);
+      } catch (error) {
+            toast.error(error.response.data.message);
+      }
+}

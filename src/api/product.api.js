af7d163e0a1a19954866b0d00730a8bf4fed8539
vishalparashar0119 <itsvishalparashar0119@gmail.com>
2026-01-11 18:33:20 +0530

    created  seperate api for filter product data

--- src/api/product.api.js
+++ src/api/product.api.js
@@ -4,18 +4,47 @@
 import axios from "axios";
 import { BACKEND_URL } from "../config/env";
 
- 
+
 
 export async function fetchAllProducts() {
-            try {
-                  const response = await axios.get(`${BACKEND_URL}/shop`, {
-                        withCredentials: true
-                  });
-                  return response.data
+      try {
+            const response = await axios.get(`${BACKEND_URL}/shop`, {
+                  withCredentials: true
+            });
+            return response.data
 
-            } catch (error) {
-                  console.log(error.message);
-                  return { success: false, message: error.response.data.message || error.message };
+      } catch (error) {
+            console.log(error.message);
+            return { success: false, message: error.response.data.message || error.message };
+      }
+
+}
+
+export const filterProductData = async (data) => {
+      try {
+            let response;
+
+            switch (data) {
+                  case "newest": response = await axios.get(`${BACKEND_URL}/products/filter/newest`, { withCredentials: true });
+                        break;
+
+                  case "popular": response = await axios.get(`${BACKEND_URL}/products/filter/popular`, { withCredentials: true });
+                        break;
+                  case "New Collection": response = await axios.get(`${BACKEND_URL}/products/filter/newest`, { withCredentials: true });
+                        break;
+                  case "All Products": response = await axios.get(`${BACKEND_URL}/shop`, { withCredentials: true });
+                        break;
+
+                  case "Discounted Products": response = await axios.get(`${BACKEND_URL}/products/filter/discount`, { withCredentials: true });
+                        break;
+
+                  case "Availability": response = await axios.get(`${BACKEND_URL}/products/filter/available`, { withCredentials: true });
+                        break;
+
             }
 
+            return response.data
+      } catch (error) {
+            return { success: false, message: error.response.data.message || error.message };
       }
+}
